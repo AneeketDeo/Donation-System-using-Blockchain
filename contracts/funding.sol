314c4221d8446dc1f7a6a57418f681918a64f32a
aneeketdeo <aneeketdeo@gmail.com>
2022-10-28 16:21:17 +0530

    Front End & Server

--- contracts/funding.sol
+++ contracts/funding.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0 < 0.9.0;
+pragma solidity >=0.6.0 < 0.9.0;
 
 contract funding{
     mapping(address=>uint) public contributors; //contributors[msg.sender]=100
@@ -22,7 +22,7 @@ contract funding{
     mapping(uint=>Request) public requests;
     uint public numRequests;
 
-    constructor (uint _target,uint _deadline) public{
+    constructor (uint _target,uint _deadline){
         target=_target;
         deadline=block.timestamp+_deadline; //10sec + 3600sec (60*60)
         minimumContribution=100 wei;
@@ -46,16 +46,16 @@ contract funding{
     }
 
 // 2. Request refund
-    // function refund() public payable{
-    //     // require(block.timestamp>deadline && raisedAmount<target,"You are not eligible for refund");
-    //     require(contributors[msg.sender]>0);
-    //     address payable user= payable(msg.sender);
-    //     user.transfer(contributors[msg.sender]);
-    //     raisedAmount -= contributors[msg.sender];
-    //     noOfContributors--;
-    //     contributors[msg.sender]=0;
+    function refund() public payable{
+        // require(block.timestamp>deadline && raisedAmount<target,"You are not eligible for refund");
+        require(contributors[msg.sender]>0);
+        address payable user= payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        raisedAmount -= contributors[msg.sender];
+        noOfContributors--;
+        contributors[msg.sender]=0;
         
-    // }
+    }
 
 // 3. To get votes from existing contributors to allocate the new project
     function voteRequest(uint _requestNo) public{
